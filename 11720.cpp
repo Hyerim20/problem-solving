dee85bd8d3f181f82f586c9d4882386dc185353a
Hyerim20 <104562082+Hyerim20@users.noreply.github.com>
2023-01-14 15:47:55 +0900

    Create 11720.cpp

---
+++ 11720.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+
+	int n;
+	cin >> n;
+	char num;
+	int sum = 0;
+	
+for (int i = 0; i < n; i++) {
+		cin >> num;
+		sum += num-48;
+	}
+	cout << sum;
+	return 0;
+}
