57f64d285be1e2da4aec1f0cfb52631679dbcbc8
Hyerim20 <104562082+Hyerim20@users.noreply.github.com>
2023-02-02 20:37:41 +0900

    Create 10809.cpp

---
+++ 10809.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	string word;
+	string alphabet = "abcdefghijklmnopqrstuvwxyz";
+	cin >> word;
+	
+	for (int i = 0; i < alphabet.length(); i++) {
+		cout << (int)word.find(alphabet[i]) << " ";
+	}
+	return 0;
+}
