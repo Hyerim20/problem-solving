a877142995411ea2ef25642d854862683ba94fa7
Hyerim20 <104562082+Hyerim20@users.noreply.github.com>
2023-03-10 18:13:06 +0900

    Create 2563.cpp

---
+++ 2563.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int arr[100][100];
+
+int main() {
+	
+	int n, x, y, cnt = 0;
+	cin >> n;
+	
+	while (n--){
+		cin >> x >> y;
+		for (int i = y; i < y + 10; i++) {
+			for (int j = x; j < x + 10; j++) {
+				if(!arr[i][j]){
+					cnt++;
+					arr[i][j] = 1;
+				}
+			}
+		}
+	}
+	cout << cnt;
+}
